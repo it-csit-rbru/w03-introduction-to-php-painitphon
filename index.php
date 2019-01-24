4a18674c105f86a9337ec323a61157d6b207f65a
painitphon <46773545+painitphon@users.noreply.github.com>
2019-01-24 16:30:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$email = "6014421011@rbru.ac.th";
+$name = "Nitiphon Bunakew";
+$url = "stu2.rbru.ac.th/~s6014421011";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+     echo "<h1>$email</h1>";
+     echo "<h2>$name</h2>";
+     echo "<a href=\$ur1\">$name</a>";
+?>
+    
+</body>
+</html>
